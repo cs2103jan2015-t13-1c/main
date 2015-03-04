bce622d6d82e0bd07b354dacb000ab209adee7bc
Huang Qin <a0085731@u.nus.edu>
2015-03-05 02:55:23 +0800

    Fixed lost integration of added command functionalities with solution.

--- ComCal_v1/ComCalManager.cpp
+++ ComCal_v1/ComCalManager.cpp
@@ -2,6 +2,44 @@
 #include "ComCalManager.h"
 
 ComCalManager::ComCalManager(){
+	std::string todoFileName;
+	std::string doneFileName;
+
+	//if (numOfArg == NOFILENAMEENTERED) {
+		todoFileName = FILENAME_TODODEFAULT;
+		doneFileName = FILENAME_DONEDEFAULT;
+	/*}
+	else if (numOfArg == TODOFILENAMEENTERED) {
+		todoFileName = argVector[1];
+		doneFileName = FILENAME_DONEDEFAULT;
+	}
+	else {
+		todoFileName = argVector[1];
+		doneFileName = argVector[2];
+	}*/
+
+	Manipulator* todoTMPtr;
+	Manipulator* doneTMPtr;
+	Logic* logPtr;
+
+	todoTMPtr = new TextManipulator(todoFileName);
+	doneTMPtr = new TextManipulator(doneFileName);
+
+	prestartCheck(todoTMPtr);
+	prestartCheck(doneTMPtr);
+	printWelcomeMessage(todoFileName, doneFileName);
+
+	logPtr = new Logic(todoTMPtr, doneTMPtr);
+
+	run(logPtr);
+
+	delete todoTMPtr;
+	delete doneTMPtr;
+	delete logPtr;
+
+	//system("pause");
+
+	return;
 }
 
 ComCalManager::~ComCalManager(){
@@ -12,4 +50,96 @@ Platform::String^ ComCalManager::deduceCommand(Platform::String^){
 	Platform::String^ feedBackMessage;
 
 	return feedBackMessage;
+}
+
+void ComCalManager::run(Logic* logPtr) {
+	_logPtr = logPtr;
+	std::string userInput;
+	std::string output;
+	bool isTerminate;
+
+	do {
+		try {
+			userInput = read();
+			isTerminate = _logPtr->executeCommand(userInput, output);
+			printOutput(output);
+		}
+		catch (std::exception& exception) {
+			std::cout << exception.what() << std::endl << std::endl;
+			if (exception.what() == TASKERROR_INVALIDSTRINGFORMAT || exception.what() == TASKERROR_INVALIDTYPECODE || exception.what() == TASKERROR_INCORRECTEMPTYATTRIBUTESYMBOL || exception.what() == TASKERROR_NODESCRIPTION || exception.what() == TASKERROR_INVALIDDATETIME || exception.what() == TASKERROR_INVALIDDATE || exception.what() == TASKERROR_INVALIDTIME || exception.what() == TASKERROR_INCORRECTTYPECODE || exception.what() == TASKERROR_MDAYOUTOFRANGELEAPFEB || exception.what() == TASKERROR_MDAYOUTOFRANGENONLEAPFEB || exception.what() == TASKERROR_MDAYOUTOFRANGE30DAYMONTH || exception.what() == TASKERROR_MDAYOUTOFRANGE31DAYMONTH || exception.what() == TASKERROR_MONTHOUTOFRANGE || exception.what() == TASKERROR_HOUROUTOFRANGE || exception.what() == TASKERROR_MINOUTOFRANGE) {
+				std::cout << "TEXT FILE IS CORRUPTED. PLEASE CHECK ActivityLog.txt FOR DETAILS." << std::endl;
+				isTerminate = true;
+			}
+			else {
+				isTerminate = false;
+			}
+		}
+	} while (!isTerminate);
+
+	return;
+}
+
+std::string ComCalManager::read(void) {
+	std::string userInput;
+
+	getline(std::cin, userInput);
+
+	if (userInput == "") {
+		throw exceptionInputNoCommand;
+	}
+
+	return userInput;
+}
+
+void ComCalManager::printOutput(std::string output) {
+	std::cout << output << std::endl;
+}
+
+void ComCalManager::prestartCheck(Manipulator* tmPtr) {
+	std::ofstream logFile;
+	time_t timeNow;
+	tm timeNowtm;
+	time(&timeNow);
+	char timeString[50];
+
+	localtime_s(&timeNowtm, &timeNow);
+	strftime(timeString, 50, "%a, %d %b %Y, %I:%M%p", &timeNowtm);
+
+	logFile.open(FILENAME_LOGDEFAULT, std::ios::app);
+	logFile << "\nPre-start Check for " << tmPtr->getFileName() << " started on: " << timeString << std::endl;
+
+	try {
+		tmPtr->sort();
+		logFile << "Pre-start Check completed with no error\n." << std::endl;
+		logFile.close();
+	}
+	catch (std::exception& exception) {
+		std::cout << "PRE-START CHECK FAILED. " << tmPtr->getFileName() << " IS CORRUPTED. PLEASE CHECK ActivityLog.txt FOR DETAILS." << std::endl;
+		logFile << "PRE-START CHECK FAILED. SEE ABOVE FOR DETAILS.\n" << std::endl;
+
+		logFile.close();
+
+		//system("pause");
+		exit(0);
+	}
+}
+
+void ComCalManager::printWelcomeMessage(std::string todoFileName, std::string doneFileName) {
+	char buffer[255];
+	std::cout << MESSAGE_WELCOME;
+	std::string output;
+
+	if (todoFileName != FILENAME_TODODEFAULT) {
+		sprintf_s(buffer, MESSAGE_TODOSPECIFIEDFILENAMECONFIRMATION.c_str(), todoFileName);
+	}
+	output = buffer;
+	std::cout << "[" << output;
+
+	if (doneFileName != FILENAME_DONEDEFAULT) {
+		sprintf_s(buffer, MESSAGE_DONESPECIFIEDFILENAMECONFIRMATION.c_str(), doneFileName);
+	}
+	output = buffer;
+	std::cout << output << "]\n\n";
+
+	return;
 }
--- ComCal_v1/ComCalManager.h
+++ ComCal_v1/ComCalManager.h
@@ -1,5 +1,46 @@
 #pragma once
+
+/***********************************************************************************************************************************************
+This Main.cpp file contains the main function, which is the entry point of the ComCal program, and serves as the program's initialiser.
+ComCal is a CLI input coupled with GUI display program that enables users to manage their tasks more efficiently and effectively.
+It's simple database of .txt files enables advanced users to more easily and conveniently edit their tasks directly in the database.
+ComCal has the following task management functionalities:
+1) (Create) add <task>: Enables users to add a task to their task list in the .txt files.
+2) (Retrieve) show <type> <date/time>: Enables users to display tasks from the .txt files tasks list based on <type> and <date/time> specified.
+3) (Update) edit <task index> <attribute> <new content>: Enables users to edit an attribute of a task in the .txt files tasks list.
+4) (Delete) del <task index>: Enables users to delete a task from the .txt files tasks list.
+5) (Done/Undone) done/undo <task index>: Enables users to mark a task from the .txt files tasks list as complete/incomplete.
+6) (Undo/Redo) undo/redo: Enables users to undo/redo last action performed/undone.
+7) (Search) search <attribute> <content>: Enables user to search for a task from the .txt files tasks list either by wildcard(without specifying any attribute) or by attribute and content specified.
+*************************************************************************************************************************************************/
+
+#include "Manipulator.h"
+#include "Logic.h"
+#include "Exception.h"
+
+#include "Windows.h"
 #include <string>
+#include <iostream>
+#include <ctime>
+
+//Default file names constant string declarations:
+const std::string FILENAME_TODODEFAULT = "TAC_todo.txt";
+const std::string FILENAME_DONEDEFAULT = "TAC_done.txt";
+
+//Default log file name constant string declarations:
+const std::string FILENAME_LOGDEFAULT = "ActivityLog.txt";
+
+//Number of file name arguments constant int declarations:
+const int NOFILENAMEENTERED = 1;
+const int TODOFILENAMEENTERED = 2;
+
+//Welcome messages constant string declarations:
+const std::string MESSAGE_WELCOME = "Welcome to TAC - your friendly Todo Assistant!\n";
+const std::string MESSAGE_TODODEFAULTFILENAMECONFIRMATION = "Working on default todo file: " + FILENAME_TODODEFAULT;
+const std::string MESSAGE_DONEDEFAULTFILENAMECONFIRMATION = ", and default done file: " + FILENAME_DONEDEFAULT;
+const std::string MESSAGE_TODOSPECIFIEDFILENAMECONFIRMATION = "Working on specified todo file: \"%s\"";
+const std::string MESSAGE_DONESPECIFIEDFILENAMECONFIRMATION = ", and specified done file: \"%s\"";
+
 ref class ComCalManager sealed
 {
 public:
@@ -11,6 +52,14 @@ public:
 	Platform::String^ deduceCommand(Platform::String^); 
 
 private:
+	Logic* _logPtr;
+
+	void prestartCheck(Manipulator*);
+	void printWelcomeMessage(std::string, std::string);
+	void run(Logic*);
+	std::string read();
+	void printOutput(std::string);
+
 	std::vector<int> indexInUse; //used to store all the index in use
 	~ComCalManager();
 
